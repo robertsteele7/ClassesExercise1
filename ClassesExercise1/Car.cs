b8a6ae7d92f71415016badbf1b72086519177ebd
robertsteele7 <atpcycle7@gmail.com>
2022-09-16 13:50:54 -0500

    first commit

---
+++ ClassesExercise1/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise1
+{
+    public class Car
+    {
+        public Car()
+        { 
+        
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public  int Year { get; set; }
+    }
+}
---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { myCar, mazda, chevy,};
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            
+            }
+        }
+    }
+}
